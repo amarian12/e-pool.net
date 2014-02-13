3e07589b1f88f2cabf4e09a3554b4eceda59eb64
Marian <amarian12@users.noreply.github.com>
2014-02-13 10:46:38 +0100

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -187,6 +187,47 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    platinum=math.Object(
+        P2P_PREFIX='28282828'.decode('hex'),
+        P2P_PORT=10125,
+        ADDRESS_VERSION=55,
+        RPC_PORT=10126,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue('platinumaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 78*100000000 >> (height + 1)//259200,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=120, # s
+        SYMBOL='PT',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'platinum') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/platinum/') if platform.system() == 'Darwin' else os.path.expanduser('~/.platinum'), 'platinum.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://platinumcrypto.com/block/index.php?block_hash=',
+        ADDRESS_EXPLORER_URL_PREFIX='http://platinumcrypto.com/address/',
+        TX_EXPLORER_URL_PREFIX='http://platinumcrypto.com/block/index.php?transaction=',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
+    Extremecoin=math.Object(
+        P2P_PREFIX='fcd9b7dd'.decode('hex'),
+        P2P_PORT=26665,
+        ADDRESS_VERSION=55,
+        RPC_PORT=26666,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'Extremecoin address' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=120, # s targetspacing
+        SYMBOL='EXC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'extremecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Extremecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Extremecoin'), 'Extremecoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://exc-blocks.pw/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://exc-blocks.pw/address/',
+        TX_EXPLORER_URL_PREFIX='http://exc-blocks.pw/tx/',
+        SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=1e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
