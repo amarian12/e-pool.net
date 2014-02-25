d8b2ec03e90aebd1c08498ab0d1773be54610fe8
Marian <amarian12@users.noreply.github.com>
2014-02-25 20:06:38 +0100

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -454,6 +454,50 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.0001,
     ),
+    continuumcoin=math.Object(
+        P2P_PREFIX='02030405'.decode('hex'), #messagestart
+        P2P_PORT=25535,
+        ADDRESS_VERSION=28, #pubkey_address
+        RPC_PORT=25536,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'continuumcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*100000000 if height<121 else 524288*100000000 >> (height * 1)//86400,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=30, # s
+        SYMBOL='CTM',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'continuumcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/continuumcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.continuumcoin'), 'continuumcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://singleblade.info/ctmcoin/block_crawler.php?block_hash=',
+        ADDRESS_EXPLORER_URL_PREFIX='http://singleblade.info/ctmcoin/block_crawler.php?address=',
+        TX_EXPLORER_URL_PREFIX='http://singleblade.info/ctmcoin/block_crawler.php?transaction=',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
+    ),
+    pwnycoin=math.Object(
+        P2P_PREFIX='04040404'.decode('hex'), #messagestart
+        P2P_PORT=26667,
+        ADDRESS_VERSION=55, #pubkey_address
+        RPC_PORT=26668,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'pwnycoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 6144*100000000 if height<8640 else 2048*100000000 >> (height * 1)//172800,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=30, # s
+        SYMBOL='PWNY',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'pwnycoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/pwnycoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.pwnycoin'), 'pwnycoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://www.pwnycoin.com/?',
+        ADDRESS_EXPLORER_URL_PREFIX='http://www.pwnycoin.com/?',
+        TX_EXPLORER_URL_PREFIX='http://www.pwnycoin.com/?',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
+    ),
+
+
 
 )
 for net_name, net in nets.iteritems():
