8fc83fd5cd35073e974ddae9ae5bb65feccd4c71
Marian <amarian12@users.noreply.github.com>
2014-02-27 16:10:51 +0100

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -496,6 +496,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    DNotes=math.Object(
+        P2P_PREFIX='f4c0aec9'.decode('hex'), #chainparams.cpp pchMessageStart
+        P2P_PORT=11224,
+        ADDRESS_VERSION=31, #PUBKEY_ADDRESS
+        RPC_PORT=11223,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'DNotesaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 250*100000000 >> (height + 1)//25900,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='NOTES',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DNotes') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/DNotes/') if platform.system() == 'Darwin' else os.path.expanduser('~/.DNotes'), 'DNotes.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://altexplorer.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://altexplorer.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://altexplorer.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
 
 
 
