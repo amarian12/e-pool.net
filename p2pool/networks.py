1b998ebcdefae7c8691c74f5a60fc4ec5cdf9559
Marian <amarian12@users.noreply.github.com>
2014-02-27 16:08:59 +0100

    Add Dnotes

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -410,6 +410,25 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    DNotes=math.Object(
+        PARENT=networks.nets['DNotes'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=3, # blocks
+        IDENTIFIER='f982abb5fab5dfdb'.decode('hex'),
+        PREFIX='f982abb5d5dfdbfd'.decode('hex'),
+        P2P_PORT=29923,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=8923,
+        BOOTSTRAP_ADDRS='p2pool.e-pool.net 107.170.35.84 note.validerrorpool.com dnotespool.com note.charityminingpools.com dnote.poolnetwork.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
+
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
