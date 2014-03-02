799eecb609c852ed4d54e76c55df9948f71c82af
Marian <amarian12@users.noreply.github.com>
2014-03-02 15:06:35 +0100

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -538,8 +538,28 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
-
-
+    darkcoin=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=9999,
+        ADDRESS_VERSION=76,
+        RPC_PORT=9998,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'darkcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda nBits, height: __import__('darkcoin_subsidy').GetBlockBaseValue(nBits, height),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('dark_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('dark_hash').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='DRK',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Darkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Darkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.darkcoin'), 'darkcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/tx/',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
