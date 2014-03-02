2053ad6d0a82fcd0588c026e8b08a4c7bd3aac36
Marian <amarian12@users.noreply.github.com>
2014-03-02 15:06:10 +0100

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -446,6 +446,26 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    darkcoin=math.Object(
+        PARENT=networks.nets['darkcoin'],
+        SHARE_PERIOD=15, # seconds
+        NEW_SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=30, # blocks
+        NEW_SPREAD=30, # blocks
+        IDENTIFIER='17cf94c1ae12e98f'.decode('hex'),
+        PREFIX='5559f46dfee6881f'.decode('hex'),
+        P2P_PORT=7902,
+        MIN_TARGET=4,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=7903,
+        BOOTSTRAP_ADDRS='p2pool.e-pool.net uswest.mine.nu'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-drk',
+        VERSION_CHECK=lambda v: True,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
